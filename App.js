1efe2a749f3ae77d339acc0bd4df26fddc231cdf
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-28 16:07:05 +0530

    done

--- App.js
+++ App.js
@@ -10,11 +10,12 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome'
 const App = () => {
 
   const [text, setText] = useState('')
+  console.log('intial:', text)
   const [items, setItems] = useState([
-    { id: Math.floor(100000 + Math.random() * 900000), text: 'Milk' },
-    { id: Math.floor(100000 + Math.random() * 900000), text: 'Milk' },
-    { id: Math.floor(100000 + Math.random() * 900000), text: 'Milk' },
-    { id: Math.floor(100000 + Math.random() * 900000), text: 'Apple' }
+    // { id: Math.floor(100000 + Math.random() * 900000), text: 'Milk' },
+    // { id: Math.floor(100000 + Math.random() * 900000), text: 'Egg' },
+    // { id: Math.floor(100000 + Math.random() * 900000), text: 'Butter' },
+    // { id: Math.floor(100000 + Math.random() * 900000), text: 'Apple' }
   ])
 
   const deleteItem = (id) => {
@@ -24,8 +25,16 @@ const App = () => {
     })
   }
 
-  const addItem = () => {
-
+  const addItem = ({ text }) => {
+    console.log(text)
+    setItems(prevItem => {
+      return [
+        {
+          id: Math.floor(10000 + Math.random() * 9000),
+          text
+        }
+        , ...prevItem]
+    })
   }
 
   const Item = ({ item, id }) => {
@@ -39,15 +48,30 @@ const App = () => {
     )
   }
 
+  const UselessTextInput = (props) => {
+    return (
+      <TextInput
+        {...props}
+        editable
+        maxLength={40}
+      />
+    )
+  }
+
   const AddItem = () => {
     return (
       <View>
-        <TextInput
+        {/* <TextInput
           placeholder="Add Item.."
           style={style.input}
-          onChange={(e) => setText(e.target.value)}
+          value={text}
+          onChangeText={t => setText(t)}
+        /> */}
+        <UselessTextInput
+          onChangeText={t => onChangeText(t)}
+          value={text}
         />
-        <TouchableOpacity style={style.btn} onPress={() => addItem}>
+        <TouchableOpacity style={style.btn} onPress={() => addItem(text)}>
           <Text style={style.btnText}>
             <Icon name="plus" />
             Add Item</Text>
